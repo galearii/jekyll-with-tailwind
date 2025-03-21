c44925e6070d00826c5518091cf6ffdffa64b56b
John Chen <galearii@gmail.com>
2025-03-21 11:22:03 +0800

    Changed defaults

---
+++ README.md
@@ -0,0 +1,24 @@
+# Jekyll with Tailwind
+Jekyll served with TailwindCSS 4 through PostCSS, based on @chrisgrande's [template](https://github.com/chrisgrande/jekyll-with-tailwind).
+
+## Build
+```
+gem install bundler
+bundle install
+npm install
+```
+
+## Run
+```
+bundle exec jekyll serve --config _config.yml,_config_dev.yml
+```
+
+## Clean
+```
+bundle exec jekyll clean
+```
+
+## TailwindCSS
+- [Tailwind](https://tailwindcss.com)
+- [Tailwind Typography](https://github.com/tailwindlabs/tailwindcss-typography)
+- [Tailwind Forms](https://github.com/tailwindlabs/tailwindcss-forms)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,46 +1,11 @@
-const colors = require("tailwindcss/colors");
-
 module.exports = {
-  content: ['./**/*.html'],
+  purge: [`_site/**/*.html`],
   theme: {
-    fontFamily: {
-      trajan: ["trajan-pro", "sans-serif"],
-    },
-    extend: {
-      animation: {
-        "spin-slow": "spin 3s linear infinite",
-      },
-      backgroundImage: (theme) => ({
-        hero: "url('/imgs/tot21_bk_hero.jpg')",
-        swirl: "url('/imgs/swirl.png')",
-        halloweenbtn: "url('/imgs/halloween_btn_bg.png')",
-        index: "url('/imgs/tot21_index_hero.jpg')",
-      }),
-    },
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      gray: colors.neutral,
-      black: colors.black,
-      white: colors.white,
-      primary: colors.orange,
-      indigo: colors.indigo,
-      totblue: {
-        DEFAULT: "#50ced4",
-      },
-      totpink: {
-        DEFAULT: "#8d005d",
-      },
-    },
-  },
-  variants: {
     extend: {},
-    variants: {
-      animation: ['motion-safe']
-    }
   },
+  variants: {},
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
   ]
-}
+};
